57f5fad23170a93770e8826f1b2122d448221ebb
DBurke <burkedion52@gmail.com>
2022-07-05 21:54:45 -0500

    portfolio

---
+++ src/components/pages/about.js
@@ -0,0 +1,32 @@
+import React from "react";
+import profilePicture from "../../../static/assets/images/auth/bio/rickandmorty81wjUb189GL._AC_SY679_.jpg";
+
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="right-column">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+        repudiandae obcaecati, labore voluptatibus eligendi odit rem dolorum,
+        nemo nesciunt inventore nulla dolores? Id molestias consectetur libero,
+        laborum blanditiis fuga pariatur? Aspernatur molestias consectetur, aut
+        quas aperiam maxime itaque. Aliquam, ipsum quibusdam, minima qui
+        consequuntur, eos a placeat tempore explicabo perspiciatis voluptates
+        quos odit obcaecati laborum repellat dignissimos incidunt aperiam
+        consequatur. Illum quo et perspiciatis laudantium nulla iusto culpa,
+        reprehenderit, sunt ratione, possimus nostrum rem porro doloribus quos
+        nesciunt vero voluptatibus asperiores officia? Natus amet ab eligendi
+        magnam quae nihil possimus. Modi, excepturi error ut voluptatem fugit
+        iure, quidem unde a impedit incidunt assumenda vero magni, similique
+        perferendis commodi optio.
+      </div>
+    </div>
+  );
+}
